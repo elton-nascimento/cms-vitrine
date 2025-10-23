cbf44954c2c568dd7ed9049b490f127304a74976
enascimento <eltonnjohnny@gmail.com>
2025-10-22 21:02:20 -0300

    fix: force type assertion for Prisma client to resolve build error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,5 +8,5 @@ const prismaClient = new PrismaClient().$extends(withAccelerate());
 
 // Exporta o cliente, mas afirmando seu tipo como PrismaClient puro.
 // Isso resolve os erros de tipo no build.
-export default prismaClient as PrismaClient;
+export default prismaClient as unknown as PrismaClient;
 
